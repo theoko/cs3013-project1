53d7c96502e78041120872f48ff7f012c345ff63
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2019-01-17 02:24:03 -0500

    Added executedCommand()

--- v0/mc0.c
+++ v0/mc0.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<sys/wait.h>
 
 #define TRUE 1
 
 int userInput(int *option);
+int checkCommand(int command);
+int executeCommand(char* command);
 
 int userInput(int *option) {
     int check = scanf("%d", option);
@@ -13,6 +18,39 @@ int userInput(int *option) {
     }
 
     return -1;
+}
+
+int executeCommand(char* command) {
+    int rc = fork();
+    
+    if(rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if(rc == 0) {
+
+    } else {
+
+    }
+}
+
+int checkCommand(int command) {
+    switch(command) {
+        case 0:
+            return executeCommand("whoami");
+            break;
+
+        case 1:
+            return executeCommand("last");
+            break;
+
+        case 2:
+            return executeCommand("ls");
+            break;
+
+        default:
+            return -1;
+            break;
+    }
 }
 
 int main(int argc, char const *argv[])
