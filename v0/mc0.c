4499d2d009c64f6753761b6ddfe74129d9cf92f3
Theodore Konstantopoulos <konstantopoulostheodore@gmail.com>
2019-01-17 02:08:04 -0500

    Reading and checking input

--- v0/mc0.c
+++ v0/mc0.c
@@ -1,8 +1,36 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+#define TRUE 1
+
+int userInput(int *option);
+
+int userInput(int *option) {
+    int check = scanf("%d", option);
+
+    if(check == 1) {
+        return 0;
+    }
+
+    return -1;
+}
+
 int main(int argc, char const *argv[])
 {
+    int *option = malloc(sizeof(int));
+
+    while(TRUE) {
+        if(userInput(option) == 0) {
+            // Input read successfully 
+            printf("%d", *option);
+        } else {
+            // Error reading input
+            // Print error message
+        }
+    }
+
+
+    free(option);
     
     return 0;
 }
